a133fab3bf53fb4d13ee26ccb8dd68ee227663e2
DanRobS <roberttsoumbou@gmail.com>
2021-01-11 20:20:46 +0100

    installing npm body-parser for POST requests

--- elastic.js
+++ elastic.js
@@ -6,6 +6,9 @@ const port = 8081;
 
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
 
@@ -84,6 +87,37 @@ app.get('/getUserById/:id', function (req, res) {
             console.log(result.body.hits.hits[0])
             return res.status(200).send(
                 result.body.hits.hits[0]
+            )
+        }
+    }); 
+})
+
+//POST METHOD TO UPDATE USER INFOS (NOM, PRENOM, ABOUT)
+app.post('/updateUserInfos', function (req, res) {
+    req.header('Content-type', 'application/json');
+    
+    console.log(req.body);
+    client.update({
+        index: 'researchers',
+        id: req.body.id,
+        body: {
+            script: {
+              source: "if(params.nom != '') {ctx._source.nom = params.nom;} if(params.prenom != '') {ctx._source.prenom = params.prenom;} if(params.about != '') {ctx._source.about = params.about;}",
+              lang: 'painless',
+              params: {
+                  nom: req.body.nom,
+                  prenom: req.body.prenom,
+                  about: req.body.about
+              }
+            }
+          }
+    }, (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            
+            return res.status(200).send(
+                
             )
         }
     }); 
